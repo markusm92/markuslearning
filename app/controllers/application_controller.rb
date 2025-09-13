13a23d44eb1e983ba810feae7bf966504007b1ca
Markus Melanen <markusmelanen@Markuss-MacBook-Pro.local>
2025-09-13 15:20:54 +0300

    Add admin controllers, views, models, Devise setup, migrations, and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,50 @@
 class ApplicationController < ActionController::Base
+    before_action : authenticate_user!
+    before_action : require_admin
+
+    def index
+        @pages = Page.all 
+    end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        @page = Page.new(page_params)
+        if @page.save
+            redirect_to admin_pages_path, notice: "Page created successfully."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @page = Page.find(params[:id])
+    end
+
+    def update
+        @page = Page.find(params[:id])
+        if @page.update(page_params)
+            redirect_to admin_pages_path, notice: "Page updated successfully."
+        else
+            render: edit, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @page = Page.find(params[:id])
+        @page.destroy
+        redirect_to admin_pages_path, notice: "Page deleted."
+    end
+
+    private
+
+    def page_params
+        params.require(:page).permit(:title, :slug, :content, :published)
+    end
+
+    def require_admin
+        redirect_to root_path, alert: "Access denied." unless current_user&.admin?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :admin do
+    resources :pages #Tää kun on lisätty, niin pitäisi näkyä index, show, new, create jne. Tarkista vielä.
+    root to: "pages#index" #Tämän pitäisi asettaa niin, että /admin menee pages dashboardille.
+  end
+
+  resources :pages, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
