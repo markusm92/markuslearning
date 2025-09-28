d08ee45fe75aea040082c785e19bd901d1a8d734
Markus Melanen <markusmelanen@Markuss-MacBook-Pro.local>
2025-09-28 17:30:19 +0300

    Changes to code and adding new parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # Homepage for logged-in users
+  authenticated :user do
+    root to: "home#index", as: :authenticated_root
+  end
+
+  # Public homepage (for visitors not logged in)
+  unauthenticated do
+    root to: "pages#show", id: 'welcome', as: :unauthenticated_root
+  end
+
+  # Admin namespace
   namespace :admin do
     resources :pages #Tää kun on lisätty, niin pitäisi näkyä index, show, new, create jne. Tarkista vielä.
     root to: "pages#index" #Tämän pitäisi asettaa niin, että /admin menee pages dashboardille.
   end
 
+  # Public content pages (show only)
   resources :pages, only: [:show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
