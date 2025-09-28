0af66e4b4009d3e718775c47cf404a5a2456ab8c
Markus Melanen <markusmelanen@Markuss-MacBook-Pro.local>
2025-09-28 18:16:48 +0300

    adding controllers home and users/sessions_controller and making changes to index and application.html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
+  before_action :authenticate_user!  # Vain sisäänkirjautuneet pitäs nähdä homepage.
   def index
+    # Fetch only published pages for users
+    @pages = Page.where(published: true).order(:title)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,19 @@
-# frozen_string_literal: true
-
+# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  # GET /users/sign_in
+  def new
+    # This sets @admin_login for the view
+    @admin_login = params[:admin] == "true"
+    super
+  end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  # POST /users/sign_in
+  def create
+    super do |resource|
+      # Redirect admins to admin dashboard
+      if resource.admin?
+        return redirect_to admin_root_path
+      end
+    end
+  end
 end
